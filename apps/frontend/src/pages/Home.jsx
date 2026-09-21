1b98fe6a053a2423f756e2667b646ebc4c13a649
ameny-rjiba <58608743+ameny-rjiba@users.noreply.github.com>
2026-09-21 11:00:59 +0100

    Update Home.jsx

--- apps/frontend/src/pages/Home.jsx
+++ apps/frontend/src/pages/Home.jsx
@@ -471,7 +471,7 @@ export default function Home() {
                     <span>{t("home.aisLearnMore")}</span>
                     <ArrowRight size={15} />
                   </Link>
-                  <a>
+                  <a
                     href="https://aisnet.org"
                     target="_blank"
                     rel="noopener noreferrer"
